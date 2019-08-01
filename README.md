ae5b8083fd35bd4efab2ffd9dbde88998028366c
daniyalnagori <daniyalnagori@yahoo.com>
2019-08-02 01:38:36 +0500

    Added more test cases for existence of: 1) HTML Input element 2) redirectionToCheckout Function Call 3) stripe constructor function

--- README.md
+++ README.md
@@ -1,22 +1,6 @@
-# Product Scaffold 
-This is a repository for a trivial challenge scaffold developed with Django contains Stripe checkout page.
-### Recommended Installation
-1. [Postgresql](https://www.postgresql.org/download/)
-2. [Python](https://www.python.org/downloads/release/python-365/)
-
-### Installation
-1. Install [pipenv](https://pypi.org/project/pipenv/)
-2. Clone this repo and `cd {{cookiecutter.project_slug}}`
-3. Run `pip install --user --upgrade pipenv` to get the latest pipenv version.
-4. Run `pipenv --python 3.6`
-5. Run `pipenv install`
-6. Run `cp .env.example .env`
-7. Update .env file `DATABASE_URL` with your `database_name`, `database_user`, `database_password`, if you use postgresql. 
-    Can alternatively set it to `sqlite:////tmp/my-tmp-sqlite.db`, if you want to use sqlite for local development.
-
-
-### Getting Started
-1. Run `pipenv shell`
-2. Run `python manage.py makemigrations`
-3. Run `python manage.py migrate`
-4. Run `python manage.py runserver`
+# Stripe Checkout Scaffold 
+This is a repository for a trivial challenge scaffold developed for Stripe checkout. 
+- Please do not change the id of HTML button element. 
+- Do not change the stripe script element. 
+- Please use provided API key.
+- You are assumed to complete the challenge in specified time limit. 
--- test.py
+++ test.py
@@ -1,22 +1,35 @@
 import unittest
+import re
 
 class TestStripe(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestStripe, self).__init__(*args, **kwargs)
         with open('order.html', 'r') as file_descriptor:
             self.dom_str = file_descriptor.read()
-        
+
+    #Check if stripe script element is present   
     def test_stripe_script_elem(self):
         stripe_script_index = self.dom_str.index('https://js.stripe.com/v3')        
-        self.assertNotEqual(stripe_script_index, -1)
+        self.assertNotEqual(stripe_script_index, -1, 'Stripe script element missing')
 
+    #Check if checkout button element is present with provided id
     def test_checkout_button(self):
         button_elem_index = self.dom_str.index('id="btn-checkout"')
-        self.assertNotEqual(button_elem_index, -1)
+        self.assertNotEqual(button_elem_index, -1, 'Checkout Button not found!')
     
+    #Check if any HTML input element is present
     def test_input_elem(self):
-        input_elem_index = self.dom_str.index('type="text')
-        self.assertNotEqual(input_elem_index, -1)
+        #input_elem_index = self.dom_str.index('type="text"')
+        self.assertRegex(self.dom_str, r'type="text"|type=\'text\'|type="number"|type=\'number\'', 'No Input Element Found')
+
+    #Check if redirectToCheckout function call is present        
+    def test_redirect_to_checkout(self):
+        self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
+    
+
+    #Check if stripe constructor function call is present   
+    def test_stripe_constructor(self):
+        self.assertNotEqual(self.dom_str, 'Stripe(', 'Stripe Constructor function call not found!')
 
 if __name__ == '__main__':
     unittest.main()
