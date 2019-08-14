1820df9283379e643415da4a44351f29b3f3e5b9
Andre Machado <csantos.machado@gmail.com>
2019-08-14 20:40:55 -0300

    Update test.py

--- test.py
+++ test.py
@@ -9,18 +9,15 @@ class TestStripe(unittest.TestCase):
 
     #Check if redirectToCheckout function call is present        
     def test_redirect_to_checkout(self):
-        return True
-        #self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
+        self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
     
     #Check if successUrl redirects to order_success.html
     def test_successUrl(self):
-        return True
-        #self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.')
+        self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.')
     
     #Check if cancelUrl redirects to order.html
     def test_cancelUrl(self):
-        return True
-        #self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.')
+        self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.')
         
 if __name__ == '__main__':
     unittest.main()
