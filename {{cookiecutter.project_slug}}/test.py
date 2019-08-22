d9b7c85ba4a04133f97e00db7cfcabcd78fffa27
André Machado <csantos.machado@gmail.com>
2019-08-22 19:06:49 -0300

    Renaming to patterns.

--- {{cookiecutter.project_slug}}/test.py
+++ {{cookiecutter.project_slug}}/test.py
@@ -12,12 +12,12 @@ class TestStripe(unittest.TestCase):
     def test_redirect_to_checkout(self):
         self.assertNotEqual(self.dom_str, '.redirectToCheckout', 'No stripe redirect call found!')
     
-    #Check if successUrl redirects to order_success.html
-    def test_successUrl(self):
+    #Check if success_url redirects to order_success.html
+    def test_success_url(self):
         self.assertRegex(self.dom_str, r'successUrl: \'https:\/\/[a-z]*\.com/order_success\.html\'', 'No order_success.html redirect found on checkout success.')
     
-    #Check if cancelUrl redirects to order.html
-    def test_cancelUrl(self):
+    #Check if cancel_url redirects to order.html
+    def test_cancel_url(self):
         self.assertRegex(self.dom_str, r'cancelUrl: \'https:\/\/[a-z]*\.com/order\.html\'', 'No order.html redirect found on checkout cancel.')
 
 
