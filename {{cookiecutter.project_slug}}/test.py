0da980452ede0d8643ae9d6caf255b352f25a62f
Andre Machado <csantos.machado@gmail.com>
2019-09-09 14:52:08 -0300

    Update test.py

--- {{cookiecutter.project_slug}}/test.py
+++ {{cookiecutter.project_slug}}/test.py
@@ -10,7 +10,7 @@ class TestAcceptanceStripe(unittest.TestCase):
 
     def test_acceptance_stripe_public_key_has_been_set(self):
         """Check if Stripe key was defined."""
-        pattern = re.compile(r"Stripe\('pk_test_\w{24}'\);", re.I | re.M)
+        pattern = re.compile(r"Stripe\('pk_test_\w+'\);", re.I | re.M)
         res = re.search(pattern, self.dom_str)
         self.assertTrue(hasattr(res, 'group'), msg="You didn't define the Stripe key.")
 
